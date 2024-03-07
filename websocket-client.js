1929ab4e8cd113d57a1d633a7e25bad7f41e9d13
Azmi SAHIN <azmisahin@outlook.com>
2024-03-07 01:31:03 +0000

    RSV1 (Reserved Bit 1)"

--- websocket-client.js
+++ websocket-client.js
@@ -1,21 +1,33 @@
 const WebSocket = require('ws');
 
-// WebSocket bağlantısı
-const socket = new WebSocket('ws://localhost:3000/api/v1/time');
-
-// Bağlantı başarılı olduğunda
-socket.on('open', () => {
-  console.log('WebSocket connection opened');
-  
-  // Mesaj gönderme
-  const message = JSON.stringify({ text: 'Hello, WebSocket!' });
-  socket.send(message);
+const wsServer = new WebSocket.Server({
+  noServer: true,
+  verifyClient: (info, next) => {
+    // Gelen mesajı düzenleyerek RSV1 bayrağını temizleme
+    info.req.rsv1 = false;
+    next(true);
+  }
 });
 
-// Mesaj alındığında
-socket.on('message', (message) => {
-  console.log('WebSocket Response:', message);
+const server = require('http').createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, HTTP!');
+});
 
-  // Bağlantıyı kapatma
-  socket.close();
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (ws) => {
+    wsServer.emit('connection', ws, request);
+  });
+});
+
+wsServer.on('connection', (socket) => {
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    socket.send(`Received your message: ${message}`);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
